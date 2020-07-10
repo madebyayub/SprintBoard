8d72e400eac4b1b455651eea6aab9b4f969bfe04
Dhruv lad <laddhruv@hotmail.com>
2020-07-09 21:53:15 -0400

    added message connection

--- src/components/dashboard/MessageBoard.js
+++ src/components/dashboard/MessageBoard.js
@@ -3,13 +3,21 @@ import io from "socket.io-client";
 import "../../stylesheets/messageboard.css";
 let socket;
 class MessageBoard extends React.Component {
+  state = {usersMessage: ''};
+
   componentDidMount() {
     socket = io('localhost:3001');
     socket.emit('join',{message: 'visited message baord'});
   }
-  componentDidUpdate(){
-    socket = io('localhost:3001');
+  sendMessage = (e) => {
+    e.preventDefault();
+    console.log(this.state.usersMessage);
+    socket.emit('join',{message: this.state.usersMessage});
+    this.setState({usersMessage: ''});
+    console.log(this.state.usersMessage);
   }
+
+
   render() {
     return (
     <div className="container-fluid">
@@ -28,10 +36,16 @@ class MessageBoard extends React.Component {
           </div>
           <div className="mainChat">
             <div className="MessageBoxContainer">
-              
+              <div className= "messageSection">
+                <div className="messageContainerright py-1 px-2">
+                  <span>hello world this is the message</span>
+                </div>
+              </div>
             </div>
               <div className="inputContainer">
-              <input  className="inputMessage mt-2" placeholder=" send  a Message..."/> 
+              <form onSubmit={(e) => this.sendMessage(e)}>
+              <input  onChange={(e) => this.setState({usersMessage: e.target.value})} value={this.state.usersMessage} className="inputMessage mt-2 px-2" placeholder="send a Message..."/> 
+              </form>
               </div>
           </div>
           <div className="mainChatRight">
